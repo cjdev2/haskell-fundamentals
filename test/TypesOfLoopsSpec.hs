5e92d68342fb2d4e77a8aabbf6a00a55e9d0028c
Alexis King <lexi.lambda@gmail.com>
2018-01-12 14:46:15 -0800

    Use foldl' and stripPrefix in TypesOfLoops

--- test/TypesOfLoopsSpec.hs
+++ test/TypesOfLoopsSpec.hs
@@ -2,29 +2,36 @@ module TypesOfLoopsSpec where
 
 import Test.Hspec
 import qualified Data.Map as Map
+import Data.List (foldl', stripPrefix)
 
 sampleLines :: [[Char]]
-sampleLines = [
-  "red",
-  "    scarlet",
-  "    ruby",
-  "    flame",
-  "green",
-  "    jade",
-  "    forest",
-  "    mint",
-  "blue",
-  "    turquoise",
-  "    azure",
-  "    sapphire"
+sampleLines =
+  [ "red"
+  , "    scarlet"
+  , "    ruby"
+  , "    flame"
+  , "green"
+  , "    jade"
+  , "    forest"
+  , "    mint"
+  , "blue"
+  , "    turquoise"
+  , "    azure"
+  , "    sapphire"
   ]
 
 type AliasMap = Map.Map [Char] [[Char]]
 
-data ColorAliasBuilder = ColorAliasBuilder { currentColor::[Char], colorAliases::AliasMap } deriving (Show, Eq)
-
 emptyColor :: [Char]
 emptyColor = "<none>"
+
+--------------------------------------------------------------------------------
+-- Fold solution
+
+data ColorAliasBuilder = ColorAliasBuilder
+  { currentColor :: [Char]
+  , colorAliases :: AliasMap
+  } deriving (Show, Eq)
 
 emptyBuilder :: ColorAliasBuilder
 emptyBuilder = ColorAliasBuilder emptyColor Map.empty
@@ -37,31 +44,40 @@ addAlias builder alias =
   let ColorAliasBuilder currentColor colorAliases = builder
       oldColorAliasList = Map.findWithDefault [] currentColor colorAliases
       newColorAliasList = oldColorAliasList ++ [alias]
-      newColorAliases = Map.insert currentColor newColorAliasList colorAliases in
-  ColorAliasBuilder currentColor newColorAliases
+      newColorAliases = Map.insert currentColor newColorAliasList colorAliases
+  in ColorAliasBuilder currentColor newColorAliases
 
 updateBuilderWithLine :: ColorAliasBuilder -> [Char] -> ColorAliasBuilder
-updateBuilderWithLine builder (' ' : ' ' : ' ' : ' ' : colorAlias) = addAlias builder colorAlias
-updateBuilderWithLine builder colorName = setCurrentColor builder colorName
+updateBuilderWithLine builder currentLine = case stripPrefix "    " currentLine of
+  Just colorAlias -> addAlias builder colorAlias
+  Nothing -> setCurrentColor builder currentLine
 
-foldColorAliasMap::[[Char]] -> AliasMap
-foldColorAliasMap lines = colorAliases $ foldl updateBuilderWithLine emptyBuilder lines
+foldColorAliasMap :: [[Char]] -> AliasMap
+-- Left folding over lists is always strict, so we should use foldl' instead of
+-- foldl to avoid space leaks. For more information see:
+-- https://wiki.haskell.org/Foldr_Foldl_Foldl%27
+foldColorAliasMap lines = colorAliases $ foldl' updateBuilderWithLine emptyBuilder lines
 
-addRemainingLines::AliasMap -> [Char] -> [[Char]] -> AliasMap
+--------------------------------------------------------------------------------
+-- Recursive solution
+
+addRemainingLines :: AliasMap -> [Char] -> [[Char]] -> AliasMap
 addRemainingLines colorAliases currentColor remainingLines =
   case remainingLines of
     currentLine : newRemainingLines ->
-      case currentLine of
-        (' ' : ' ' : ' ' : ' ' : alias) ->
+      case stripPrefix "    " currentLine of
+        Just alias ->
           let oldColorAliasList = Map.findWithDefault [] currentColor colorAliases
               newColorAliasList = oldColorAliasList ++ [alias]
-              newColorAliases = Map.insert currentColor newColorAliasList colorAliases in
-          addRemainingLines newColorAliases currentColor newRemainingLines
-        newColor -> addRemainingLines colorAliases newColor newRemainingLines
+              newColorAliases = Map.insert currentColor newColorAliasList colorAliases
+          in addRemainingLines newColorAliases currentColor newRemainingLines
+        Nothing -> addRemainingLines colorAliases currentLine newRemainingLines
     _ -> colorAliases
 
-recurseColorAliasMap::[[Char]] -> AliasMap
+recurseColorAliasMap :: [[Char]] -> AliasMap
 recurseColorAliasMap lines = addRemainingLines Map.empty emptyColor lines
+
+--------------------------------------------------------------------------------
 
 spec :: Spec
 spec = do
@@ -80,17 +96,21 @@ spec = do
     addAlias redBuilder "scarlet" `shouldBe` ColorAliasBuilder "red" (Map.fromList [("red", ["scarlet"])])
 
   it "update builder with line" $ do
-    (updateBuilderWithLine emptyBuilder "red") `shouldBe` ColorAliasBuilder "red" (Map.fromList [])
-    (updateBuilderWithLine (ColorAliasBuilder "red" (Map.fromList [])) "    scarlet") `shouldBe` ColorAliasBuilder "red" (Map.fromList [("red", ["scarlet"])])
+    updateBuilderWithLine emptyBuilder "red"
+      `shouldBe` ColorAliasBuilder "red" (Map.fromList [])
+    updateBuilderWithLine (ColorAliasBuilder "red" (Map.fromList [])) "    scarlet"
+      `shouldBe` ColorAliasBuilder "red" (Map.fromList [("red", ["scarlet"])])
 
   it "fold color alias map" $ do
-    (foldColorAliasMap sampleLines) `shouldBe` Map.fromList[
-      ("red", ["scarlet", "ruby", "flame"]),
-      ("green", ["jade", "forest", "mint"]),
-      ("blue", ["turquoise", "azure", "sapphire"])]
+    foldColorAliasMap sampleLines `shouldBe` Map.fromList
+      [ ("red", ["scarlet", "ruby", "flame"])
+      , ("green", ["jade", "forest", "mint"])
+      , ("blue", ["turquoise", "azure", "sapphire"])
+      ]
 
   it "recurse color alias map" $ do
-    (recurseColorAliasMap sampleLines) `shouldBe` Map.fromList[
-      ("red", ["scarlet", "ruby", "flame"]),
-      ("green", ["jade", "forest", "mint"]),
-      ("blue", ["turquoise", "azure", "sapphire"])]
+    recurseColorAliasMap sampleLines `shouldBe` Map.fromList
+      [ ("red", ["scarlet", "ruby", "flame"])
+      , ("green", ["jade", "forest", "mint"])
+      , ("blue", ["turquoise", "azure", "sapphire"])
+      ]
