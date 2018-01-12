8dfbba345569ed0daba1e8bdd34ee3ffea87b44b
Alexis King <lexi.lambda@gmail.com>
2018-01-12 14:56:06 -0800

    Fix directory casing issues due to case-insensitivity on macOS

---
+++ src/Maintainability/Unmaintainable/Main.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Maintainability.Unmaintainable.Main
+  ( main
+  ) where
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+import Prelude hiding (log, readFile)
+
+import Data.Semigroup ((<>))
+import Data.Time.Clock (diffUTCTime, getCurrentTime)
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  startTime <- getCurrentTime
+  [fileName] <- getArgs
+  target <- T.readFile fileName
+  T.putStrLn $ "Hello, " <> target <> "!"
+  endTime <- getCurrentTime
+  let duration = endTime `diffUTCTime` startTime
+  T.putStrLn $ T.pack (show (round (duration * 1000) :: Integer)) <> " milliseconds"
