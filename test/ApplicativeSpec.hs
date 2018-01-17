07fbd3bb515e78a420a916d124808c150ce67ad3
Sean Shubin <sshubin@cj.com>
2018-01-17 15:35:40 -0800

    More applicative and functor examples

--- test/ApplicativeSpec.hs
+++ test/ApplicativeSpec.hs
@@ -2,6 +2,7 @@ module ApplicativeSpec where
 
 import Test.Hspec
 import Control.Monad.State (runState)
+import Control.Applicative
 
 spec :: Spec
 spec = do
@@ -38,5 +39,43 @@ spec = do
       pure True `shouldReturnInIO` True
       pure "hello" `shouldReturnInIO` "hello"
 
+  describe "(<*>)" $ do
+    -- (<$>) is an infix alias for fmap
+    -- (<*>) is pronounced "apply" (it has no prefix alias)
 
--- TODO: demonstrate (<*>)
+    it "applies a function wrapped in Just to a value wrapped in Just" $ do
+      Just (+ 1) <*> Nothing `shouldBe` Nothing
+      Just (+ 1) <*> (Just 2) `shouldBe` Just 3
+
+    it "using case...of to perform function application through Maybe values" $ do
+      let addTwoMaybeIntegers :: Maybe Integer -> Maybe Integer -> Maybe Integer
+          addTwoMaybeIntegers (Just x) (Just y) = Just (x + y)
+          addTwoMaybeIntegers _        _        = Nothing
+
+      addTwoMaybeIntegers (Just 1) (Just 2) `shouldBe` Just 3
+      addTwoMaybeIntegers Nothing (Just 2) `shouldBe` Nothing
+      addTwoMaybeIntegers (Just 1) Nothing `shouldBe` Nothing
+      addTwoMaybeIntegers Nothing Nothing `shouldBe` Nothing
+
+    it "(<*>) works with (<$>) to plumb function application through Maybe values" $ do
+      ((+) <$> Just 1 <*> Just 2) `shouldBe` Just 3
+      ((+) <$> Nothing <*> Just 2) `shouldBe` Nothing
+      ((+) <$> Just 1 <*> Nothing) `shouldBe` Nothing
+      ((+) <$> Nothing <*> Nothing) `shouldBe` Nothing
+
+    it "using case...of to perform function application through Either values" $ do
+      let addTwoEitherIntegers :: Either String Integer -> Either String Integer -> Either String Integer
+          addTwoEitherIntegers (Right x) (Right y) = Right (x + y)
+          addTwoEitherIntegers (Left x)  _         = Left x
+          addTwoEitherIntegers _         (Left x)  = Left x
+
+      addTwoEitherIntegers (Right 1) (Right 2) `shouldBe` Right 3
+      addTwoEitherIntegers (Left "err") (Right 2) `shouldBe` Left "err"
+      addTwoEitherIntegers (Right 1) (Left "err") `shouldBe` Left "err"
+      addTwoEitherIntegers (Left "err 1") (Left "err 2") `shouldBe` Left "err 1"
+
+    it "(<*>) works with (<$>) to plumb function application through Either values" $ do
+      ((+) <$> Right 1 <*> Right 2) `shouldBe` (Right 3 :: Either String Integer)
+      ((+) <$> Left "err" <*> Right 2) `shouldBe` Left "err"
+      ((+) <$> Right 1 <*> Left "err") `shouldBe` Left "err"
+      ((+) <$> Left "err 1" <*> Left "err 2") `shouldBe` Left "err 1"
--- test/FunctorSpec.hs
+++ test/FunctorSpec.hs
@@ -4,5 +4,66 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  it "work in progress, using vacuous test as placeholder" $ do
-    True `shouldBe` True
+  describe "fmap is a tool for function composition" $ do
+    -- fmap :: Functor f => (a -> b) -> (f a -> f b)
+    -- examples of functors are Maybe, List, Either
+    -- these also happen to be monads
+    -- all monads are functors
+    -- not all functors are monads
+
+    it "fmap lifts pure functions to work on values inside Maybe" $ do
+      let intToString :: Integer -> String
+          intToString = show
+
+          liftedIntToString :: Maybe Integer -> Maybe String
+          liftedIntToString = fmap intToString
+
+      liftedIntToString (Just 42) `shouldBe` Just "42"
+      liftedIntToString Nothing `shouldBe` Nothing
+
+    it "fmap lifts pure functions to work on values inside Either with concrete types" $ do
+      let intToString :: Integer -> String
+          intToString = show
+
+          liftedIntToString :: Either String Integer -> Either String String
+          liftedIntToString = fmap intToString
+
+      liftedIntToString (Right 42) `shouldBe` Right "42"
+      liftedIntToString (Left "error") `shouldBe` Left "error"
+
+    it "fmap lifts pure functions to work on values inside Either with polymorphic type" $ do
+      let intToString :: Integer -> String
+          intToString = show
+
+          liftedIntToString :: Either a Integer -> Either a String
+          liftedIntToString = fmap intToString
+
+      liftedIntToString (Right 42) `shouldBe` (Right "42" :: Either String String)
+      liftedIntToString (Left "error") `shouldBe` Left "error"
+
+    it "fmap lifts pure functions to work on values inside lists" $ do
+      let intToString :: Integer -> String
+          intToString = show
+
+          liftedIntToString :: [Integer] -> [String]
+          liftedIntToString = fmap intToString
+
+      liftedIntToString [42] `shouldBe` ["42"]
+      liftedIntToString [1, 2, 3] `shouldBe` ["1", "2", "3"]
+      liftedIntToString [] `shouldBe` []
+
+
+    it "fmap lifts pure functions to work on any functor" $ do
+      let intToString :: Integer -> String
+          intToString = show
+
+          liftedIntToString :: Functor f => f Integer -> f String
+          liftedIntToString = fmap intToString
+
+      liftedIntToString (Just 42) `shouldBe` Just "42"
+      liftedIntToString Nothing `shouldBe` Nothing
+      liftedIntToString (Right 42) `shouldBe` (Right "42" :: Either String String)
+      liftedIntToString (Left "error") `shouldBe` Left "error"
+      liftedIntToString [42] `shouldBe` ["42"]
+      liftedIntToString [1, 2, 3] `shouldBe` ["1", "2", "3"]
+      liftedIntToString [] `shouldBe` []
