ef73a7e4fceb823f4cce5d1ba31a6c0d9e1f4264
Sean Shubin <sshubin@cj.com>
2018-01-17 11:37:39 -0800

    Added examples for applicative and monad

--- test/ApplicativeSpec.hs
+++ test/ApplicativeSpec.hs
@@ -1,8 +1,42 @@
 module ApplicativeSpec where
 
 import Test.Hspec
+import Control.Monad.State (runState)
 
 spec :: Spec
 spec = do
-  it "work in progress, using vacuous test as placeholder" $ do
-    True `shouldBe` True
+  describe "pure" $ do
+    it "for Maybe, wraps a value in Just" $ do
+      pure () `shouldBe` Just ()
+      pure True `shouldBe` Just True
+      pure "hello" `shouldBe` Just "hello"
+
+    it "for Either, wraps a value in Right" $ do
+      -- Left side can be anything, here we specify String with a type annotation to avoid ambiguity
+      pure () `shouldBe` (Right () :: Either String ())
+      pure True `shouldBe` (Right True :: Either String Bool)
+      pure "hello" `shouldBe` (Right "hello" :: Either String String)
+
+    it "for list, wraps a value in a single-element list" $ do
+      pure () `shouldBe` [()]
+      pure True `shouldBe` [True]
+      pure "hello" `shouldBe` ["hello"]
+
+    it "for State, ignores the current state and leaves it unchanged" $ do
+      runState (pure ()) (42 :: Integer) `shouldBe` ((), 42)
+      runState (pure True) (42 :: Integer) `shouldBe` (True, 42)
+      runState (pure "hello") (42 :: Integer) `shouldBe` ("hello", 42)
+
+    it "for I/O, creates a computation that does no side-effects" $ do
+      -- evaluates its argument in the IO monad and checks its result
+      let shouldReturnInIO :: (Eq a, Show a) => IO a -> a -> IO ()
+          shouldReturnInIO action value = do
+            result <- action
+            result `shouldBe` value
+
+      pure () `shouldReturnInIO` ()
+      pure True `shouldReturnInIO` True
+      pure "hello" `shouldReturnInIO` "hello"
+
+
+-- TODO: demonstrate (<*>)
--- test/MonadSpec.hs
+++ test/MonadSpec.hs
@@ -2,14 +2,66 @@ module MonadSpec where
 
 import Test.Hspec
 
+-- A Rational is a ratio of two Integer values
+safeDivide :: Rational -> Rational -> Maybe Rational
+safeDivide _ 0 = Nothing
+safeDivide a b = Just (a / b)
+
+-- split a list into its head and tail
+uncons :: [a] -> Maybe (a, [a])
+uncons [] = Nothing
+uncons (x:xs) = Just (x, xs)
+
 spec :: Spec
 spec = do
   it "work in progress, using vacuous test as placeholder" $ do
     True `shouldBe` True
 
+  it "divide first element by the second element with nested case...of" $ do
+    let numbers = [1, 3, 5]
+        quotient = case uncons numbers of
+          Just (a, as) -> case uncons as of
+            Just (b, _) ->
+              safeDivide a b
+            Nothing -> Nothing
+          Nothing -> Nothing
+    quotient `shouldBe` Just (1/3)
+
+  it "divide first element by the second element with >>=" $ do
+    let numbers = [1, 3, 5]
+        quotient =
+          uncons numbers >>= \(a, as) ->
+            uncons as >>= \(b, _) ->
+              safeDivide a b
+    quotient `shouldBe` Just (1/3)
+
+  it "divide first element by the second element with do notation" $ do
+    let numbers = [1, 3, 5]
+        quotient = do
+          (a, as) <- uncons numbers
+          (b, _) <- uncons as
+          safeDivide a b
+    quotient `shouldBe` Just (1/3)
+
+  -- pure lifts the tuple into the list context
+  -- would be the same thing if you wrapped it in a single element list like this: [(x, y)]
+  -- see also ApplicativeSpec
+  it "the list monad produces a cartesian product with >>=" $ do
+    let tuples =
+          [1, 3, 5] >>= \x ->
+            [2, 4] >>= \y ->
+              pure (x, y)
+    tuples `shouldBe` [(1, 2), (1, 4), (3, 2), (3, 4), (5, 2), (5, 4)]
+
+  it "the list monad produces a cartesian product with do notation" $ do
+    let tuples = do
+          x <- [1, 3, 5]
+          y <- [2, 4]
+          pure (x, y)
+    tuples `shouldBe` [(1, 2), (1, 4), (3, 2), (3, 4), (5, 2), (5, 4)]
+
 -- todo: implement examples of the following
--- >>= vs. do
--- io, maybe, list, state
+-- io, list, state
 -- lift vs. ap
 -- return, fail, guard, mzero
 -- fmap, join
